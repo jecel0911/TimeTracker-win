94f0b76155470e1e1f91ed4c531488946756893e
Jeremias <jceciliano@soin.co.cr>
2016-02-15 12:58:36 -0600

    Adding some components

--- TimeTracker/Form1.cs
+++ TimeTracker/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -36,6 +37,24 @@ namespace TimeTracker
         {
             e.Cancel = true;
             this.Hide();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(BackColor);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+            //this.BackColor = Color.LimeGreen;
+            //this.TransparencyKey = Color.LimeGreen;
+            
+            //System.Drawing.Drawing2D.GraphicsPath shape = new System.Drawing.Drawing2D.GraphicsPath();
+            //shape.AddEllipse(0, 0, this.Width*2, this.Height*2);
+            //this.Region = new System.Drawing.Region(shape);
         }
     }
 }
