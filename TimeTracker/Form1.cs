b8601d3705177812ac277ca819bf2434b18d6a02
Jeremias <jceciliano@soin.co.cr>
2016-04-13 17:11:35 -0600

    adding global keyhooks for windows

--- TimeTracker/Form1.cs
+++ TimeTracker/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Windows.Forms;
 
@@ -12,9 +13,24 @@ namespace TimeTracker
 {
     public partial class Form1 : Form
     {
+        KeyboardHook hook = new KeyboardHook();
+
         public Form1()
         {
             InitializeComponent();
+
+            // register the event that is fired after the key press.
+            hook.KeyPressed +=
+                new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            // register the control + alt + F12 combination as hot key.
+            hook.RegisterHotKey(TimeTracker.ModifierKeys.Control | TimeTracker.ModifierKeys.Shift,
+                Keys.T);
+        }
+
+        void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            // show the keys pressed in a label.
+            Console.WriteLine("Control + Alt + T pressed");
         }
 
         private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
@@ -56,5 +72,11 @@ namespace TimeTracker
             //shape.AddEllipse(0, 0, this.Width*2, this.Height*2);
             //this.Region = new System.Drawing.Region(shape);
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
---
+++ TimeTracker/utils/HotKeysRegister.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using Trustsoft.WideHotkeys;
+//using NHotkey;
+
+namespace TimeTracker.utils
+{
+    class HotKeysRegister
+    {
+        private HotkeyManager hkm = new HotkeyManager();
+        public HotKeysRegister()
+        {
+            registerDefaultHotKeys();
+        }
+
+        private void registerDefaultHotKeys()
+        {
+
+        }
+
+       
+}
+
+}
+
+
+/*
+
+
+     var hkm = new HotkeyManager();
+        hkm.AddOrReplace("nombre", Keys.Control | Keys.Shift | Keys.Down, OnHotkey1);
+        }
+
+    private void OnHotkey1(object sender, HotkeyEventArgs e)
+    {
+        Console.WriteLine("Ctrl+Shift+ Hotkey pressed");
+
+        e.Handled = true;
+    }
+
+
+    */
